0b78a85618a50c384fa79c5b4bfdc4c2664e220b
William Kennedy <wkennedy@teamhuber.com>
2013-08-19 18:42:30 -0600

    filter

---
+++ config-paths.h
@@ -0,0 +1,13 @@
+/* this is a generated file - do not edit */
+
+/* gettext domain used for menu translations */
+/* #undef MENU_TEXTDOMAIN "" */
+
+/* define an extra path for pixmaps */
+#define PIXMAPDIR "/usr/local/share/pixmaps"
+
+/* where shared data is stored */
+#define PKGDATADIR "/usr/local/share/Workspace"
+
+/* where the configuration is stored */
+#define SYSCONFDIR "/usr/local/etc"
---
+++ config.h
@@ -0,0 +1,207 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <bsd/string.h> header file. */
+/* #undef HAVE_BSD_STRING_H */
+
+/* Define to 1 if you have the <ctype.h> header file. */
+#define HAVE_CTYPE_H 1
+
+/* Define to 1 if you have the declaration of `sys_siglist', and to 0 if you
+   don't. */
+#define HAVE_DECL_SYS_SIGLIST 0
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
+/* #undef HAVE_DOPRNT */
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `gethostname' function. */
+#define HAVE_GETHOSTNAME 1
+
+/* Check for inotify */
+#define HAVE_INOTIFY 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <libintl.h> header file. */
+#define HAVE_LIBINTL_H 1
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* Define to 1 if you have the `mallinfo' function. */
+#define HAVE_MALLINFO 1
+
+/* Define to 1 if you have the <malloc.h> header file. */
+#define HAVE_MALLOC_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `mkstemp' function. */
+#define HAVE_MKSTEMP 1
+
+/* Define to 1 if you have the `poll' function. */
+#define HAVE_POLL 1
+
+/* Define to 1 if you have the <poll.h> header file. */
+#define HAVE_POLL_H 1
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have the `setsid' function. */
+#define HAVE_SETSID 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strcasecmp' function. */
+#define HAVE_STRCASECMP 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if strlcpy is available */
+/* #undef HAVE_STRLCAT */
+
+/* Define to 1 if you have the `strncasecmp' function. */
+#define HAVE_STRNCASECMP 1
+
+/* Define to 1 if you have the `sysconf' function. */
+#define HAVE_SYSCONF 1
+
+/* Define to 1 if you have the <sys/inotify.h> header file. */
+#define HAVE_SYS_INOTIFY_H 1
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have <sys/wait.h> that is POSIX.1 compatible. */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `vprintf' function. */
+#define HAVE_VPRINTF 1
+
+/* define if your X server has XConvertCase() (set by configure) */
+#define HAVE_XCONVERTCASE 1
+
+/* define if your X server has XInternAtoms() (set by configure) */
+#define HAVE_XINTERNATOMS 1
+
+/* define if you want support for XRandR (set by configure) */
+/* #undef HAVE_XRANDR */
+
+/* Internationalization (I18N) support (set by configure) */
+/* #undef I18N */
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "Workspace"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Workspace"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Workspace 0.95.4"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "Workspace"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.95.4"
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* define if you want support for shaped windows (set by configure) */
+#define SHAPE 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* define if you want user defined menus for applications */
+/* #undef USER_MENU */
+
+/* Define if Boehm GC is to be used */
+/* #undef USE_BOEHM_GC */
+
+/* define if GIF libraries are available (set by configure) */
+/* #undef USE_GIF */
+
+/* define if JPEG libraries are available (set by configure) */
+#define USE_JPEG 1
+
+/* define if PNG libraries are available (set by configure) */
+#define USE_PNG 1
+
+/* define if TIFF libraries are available (set by configure) */
+#define USE_TIFF 1
+
+/* define if XPM libraries are available (set by configure) */
+#define USE_XPM 1
+
+/* Version number of package */
+#define VERSION "0.95.4"
+
+/* define if you want support for the XINERAMA extension (set by configure) */
+/* #undef XINERAMA */
+
+/* whether XKB language MODELOCK should be enabled */
+/* #undef XKB_MODELOCK */
+
+/* define if X's shared memory extension is available (set by configure) */
+#define XSHM 1
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
+
+/* define if you want support for X window's X_LOCALE (set by configure) */
+/* #undef X_LOCALE */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef pid_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+#include "config-paths.h"
