c16d563957631eabaff65b65a98733c522cf0c27
William Kennedy <wkennedy@teamhuber.com>
2013-08-19 18:29:42 -0600

    setup.sh - message change

--- setup.sh
+++ setup.sh
@@ -15,6 +15,9 @@ automake -a --gnu --include-deps
 ./configure && make
 
 echo "-----------------------------"
+echo "If everything went OK,"
 echo "Run 'make install' as root."
+echo "If it *didn't*, run 'make clean'"
+echo "and re-run setup.sh."
 echo "-----------------------------"
 echo ; echo ; echo
